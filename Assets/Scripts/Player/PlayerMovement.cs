c49429c8347a30147b5b9d05d90eae12b5194312
SaherPersonal <saher.ziauddin@dal.ca>
2022-09-22 20:34:32 -0300

    - implemented jumping capabilities

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,29 +7,71 @@ public class PlayerMovement : MonoBehaviour
 {
     public new Rigidbody rigidbody;
     public float speed = 4f;
+    public float gravityScale = -9.81f;
+    public float jumpVelocity = 10f;
+    public float fallingGravityScale = 15f;
+    public float turnSmoothTime = 0.1f;
+    
     private Vector3 direction;
-    private bool isMoving = false;
+    private bool isJumping = false;
+    private Vector3 verticalVelocity;
+    private float turnSmoothVelocity;
+
+    private void Start()
+    {
+        verticalVelocity = new Vector3(0, jumpVelocity, 0);
+    }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        direction = new Vector3(horizontal, 0f, vertical);
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
             //transform.forward = direction;
             float targetAngle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-            isMoving = true;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
         }
-        else { isMoving = false; }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
+
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() // called every second
     {
-        if (!isMoving) return;
-        rigidbody.MovePosition(rigidbody.position + direction * speed * Time.fixedDeltaTime); 
+        rigidbody.MovePosition(rigidbody.position + direction * speed * Time.fixedDeltaTime);
+        
+        if (isJumping)
+        {
+            if (rigidbody.velocity.y >= 0)
+            {
+                verticalVelocity = verticalVelocity + (new Vector3(0f, gravityScale * Time.fixedDeltaTime, 0f));
+                rigidbody.velocity = verticalVelocity;
+            }
+
+            if(rigidbody.velocity.y < 0)
+            {
+                verticalVelocity = verticalVelocity + (new Vector3(0f, fallingGravityScale * Time.fixedDeltaTime, 0f));
+                rigidbody.velocity = verticalVelocity;
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            isJumping = false;
+            verticalVelocity = new Vector3(0, jumpVelocity, 0);
+            Debug.Log("Collision with ground detected");
+        }
     }
 }
