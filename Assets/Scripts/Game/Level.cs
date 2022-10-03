be7e091c625169bb7c417de53c4034c3439f12ee
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-02 22:59:22 -0300

    - Finished camera shake - Created Prefabs

---
+++ Assets/Scripts/Game/Level.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public List<GameObject> checkPoints = new List<GameObject>();
+
+    public void LoadLevel()
+    {
+
+    }
+
+    public void CheckIfPlayerWon()
+    {
+
+    }
+
+    public void RespawnAtLastCheckPoint()
+    {
+
+    }
+
+    public void RestartLevel()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    enum PLAYER_STATE
+    {
+        IDLE,
+        MOVING,
+        JUMPING_UP,
+        FALLING_DOWN,
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Lava"))
+        {
+            PlayerDeath();
+        }
+    }
+
+    public void PlayerDeath()
+    {
+
+    }
+}
