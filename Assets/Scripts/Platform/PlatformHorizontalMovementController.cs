3f50f0efb1643e6d8271da6edd8e692ea5f32e13
SaherPersonal <saher.ziauddin@dal.ca>
2022-10-02 22:12:18 -0300

    - implemented horizontal and vertical movement for platforms

---
+++ Assets/Scripts/Platform/PlatformHorizontalMovementController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformHorizontalMovementController : MonoBehaviour
+{
+    public float horizontalRightLimit = 5;
+    public float horizontalLeftLimit = -5;
+    public float moveSpeed = 4f;
+
+    private int directionToMove = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > horizontalRightLimit)
+        {
+            directionToMove = -1;
+        }
+        else if (transform.position.x < horizontalLeftLimit)
+        {
+            directionToMove = 1;
+        }
+
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        Vector3 horizontalMovement = Vector3.right * directionToMove * moveSpeed * Time.deltaTime;
+        transform.Translate(horizontalMovement);
+    }
+}
